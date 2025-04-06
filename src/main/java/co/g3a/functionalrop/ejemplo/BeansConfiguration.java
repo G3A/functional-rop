60d203e7411547c1452744a27ec78224594d861e
G3A <tavo51@hotmail.com>
2025-04-05 22:49:42 -0500

    Correccion del nombre de la clase

--- src/main/java/co/g3a/functionalrop/ejemplo/BeansConfiguration.java
+++ src/main/java/co/g3a/functionalrop/ejemplo/BeansConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BeansCofiguration {
+public class BeansConfiguration {
 
     @Bean
     public ErrorMessageProvider errorMessageProvider(){
